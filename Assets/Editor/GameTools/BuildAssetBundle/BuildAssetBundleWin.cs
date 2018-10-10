0f5c0788cf6fb95f6844853555e38d1bf8ba1053
tongguang <591912867@qq.com>
2018-10-10 22:08:43 +0800

    工具提交

---
+++ Assets/Editor/GameTools/BuildAssetBundle/BuildAssetBundleWin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+
+namespace U3dClient.GameTools
+{
+    public class BuildAssetBundleWin: OdinEditorWindow
+    {
+        public static void OpenWindow()
+        {
+            var window = GetWindow<BuildAssetBundleWin>();
+            window.position = GUIHelper.GetEditorWindowRect().AlignCenter(700, 700);
+        }
+
+        public string RawResRootPath = "";
+    }
+}
---
+++ Assets/Editor/GameTools/GameToolsEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace U3dClient.GameTools
+{
+    public static class GameToolsEditor
+    {
+        [MenuItem("GameTools/OpenBuildAssetBundleWin")]
+        public static void OpenBuildAssetBundleWin()
+        {
+            BuildAssetBundleWin.OpenWindow();
+        }
+
+        [MenuItem("GameTools/Test")]
+        public static void Test()
+        {
+            Debug.Log(Path.GetPathRoot("test"));
+        }
+    }
+}
