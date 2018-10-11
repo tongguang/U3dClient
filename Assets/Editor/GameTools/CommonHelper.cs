6dc2029ad3f1e3c77d84a5b594887830d80d14fa
tongguang <591912867@qq.com>
2018-10-11 21:46:26 +0800

    编辑器帮助函数

---
+++ Assets/Editor/GameTools/CommonHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace U3dClient.GameTools
+{
+    public static class CommonHelper
+    {
+        public static string NormalPath(string path)
+        {
+            return path.Replace("\\", "/");
+        }
+
+        public static string CombinePath(string path1, string path2)
+        {
+            return NormalPath(Path.Combine(path1, path2));
+        }
+    }
+}
