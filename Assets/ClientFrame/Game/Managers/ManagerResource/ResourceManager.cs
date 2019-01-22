ebbb8635e47632ff71610c2884a9014f23a86cdd
tongguang <591912867@qq.com>
2019-01-22 15:36:28 +0800

    资源加载封装

--- Assets/ClientFrame/Game/Managers/ManagerResource/ResourceManager.cs
+++ Assets/ClientFrame/Game/Managers/ManagerResource/ResourceManager.cs
@@ -2,11 +2,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace U3dClient
 {
     public class ResourceManager : IGameManager
     {
+        private static int s_ResourceIndex = 0;
+
+        public static int GetNewResourceIndex()
+        {
+            return s_ResourceIndex++;
+        }
+
+//        private GameConfig.AssetLoadModeEnum AssetLoadMode;
+
+        public void InitBundleManifest()
+        {
+            FullBundleLoader.InitBundleManifest();
+        }
+
+        public int LoadAssetAsync<T>(string bundleName, string assetName, Action<bool, T> loadedAction) where T : Object
+        {
+            var assetLoadMode = GameCenter.s_ConfigManager.GlobalGameConfig.AssetLoadMode;
+#if UNITY_EDITOR
+            if (assetLoadMode == GameConfig.AssetLoadModeEnum.EditMode)
+            {
+                return EditorModeAssetLoader.LoadAsync<T>(bundleName, assetName, loadedAction);
+            }
+            else
+#endif
+            {
+                return BundleAssetLoader.LoadAsync<T>(bundleName, assetName, loadedAction);
+            }
+        }
+
+        public int LoadAssetSync<T>(string bundleName, string assetName, Action<bool, T> loadedAction) where T : Object
+        {
+            var assetLoadMode = GameCenter.s_ConfigManager.GlobalGameConfig.AssetLoadMode;
+#if UNITY_EDITOR
+            if (assetLoadMode == GameConfig.AssetLoadModeEnum.EditMode)
+            {
+                return EditorModeAssetLoader.LoadSync<T>(bundleName, assetName, loadedAction);
+            }
+            else
+#endif
+            {
+                return BundleAssetLoader.LoadSync<T>(bundleName, assetName, loadedAction);
+            }
+        }
+
+        public void UnLoadAsset(int resourceIndex)
+        {
+            var assetLoadMode = GameCenter.s_ConfigManager.GlobalGameConfig.AssetLoadMode;
+#if UNITY_EDITOR
+            if (assetLoadMode == GameConfig.AssetLoadModeEnum.EditMode)
+            {
+                EditorModeAssetLoader.UnLoad(resourceIndex);
+            }
+            else
+#endif
+            {
+                BundleAssetLoader.UnLoad(resourceIndex);
+            }
+        }
+
         public void Awake()
         {
         }
@@ -29,18 +89,6 @@ namespace U3dClient
 
         public void OnApplicationQuit()
         {
-        }
-
-        private static int s_ResourceIndex = 0;
-
-        public static int GetNewResourceIndex()
-        {
-            return s_ResourceIndex++;
-        }
-
-        public void InitBundleManifest()
-        {
-            FullBundleLoader.InitBundleManifest();
         }
     }
 }
